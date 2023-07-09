201b1943332207a9a657dd501ad6cc39e2236a23
kckirch <kckircher@hotmail.com>
2023-07-09 16:24:43 -0600

    almost looks how I want, playing with transparency

--- components/Start/pages/SignUpPage.js
+++ components/Start/pages/SignUpPage.js
@@ -1,54 +1,58 @@
-import { View } from 'react-native';
-import { useState } from 'react';
-import _Button from '../elements/_Button';
-import _Input from '../elements/_Input';
+import React, { useState } from 'react';
+import { Modal, View, TouchableWithoutFeedback } from 'react-native';
 import _Header from '../elements/_Header';
-import _Divider from '../elements/_Divider';
 import _Navigator from '../elements/_Navigator';
 import STYLES from '../styles/Styles';
-import TERMS from '../../../settings/Terms'
-terms = TERMS['English']
+import TERMS from '../../../settings/Terms';
 import EmailPassword from './EmailPassword';
 import Google from './Google';
 import Facebook from './Facebook';
 
+const terms = TERMS['English'];
 
 export default SignUpPage = props => {
-   const [mainScreenState, setMainScreenState] = useState('EmailPassword');
+  const [mainScreenState, setMainScreenState] = useState('EmailPassword');
+  const [modalVisible, setModalVisible] = useState(true);
 
-   return (
-      <View
-      style={STYLES.page}
-      >
-         <View
-            style={STYLES.suliSpacer}
-         >
-         </View>
-         <View
-            style={STYLES.suliContainer}
-         >
-            <_Header
-               text={terms['0005']}
-               action={() => props.setPageScreenState('StartPage')}
-            />
-            <_Navigator 
-               screens={{
-                  'EmailPassword': <EmailPassword
-                     setPageScreenState={(_) => props.setPageScreenState(_)}
-                     setMainScreenState={(_) => setMainScreenState(_)}
-                  />,
-                  'Google': <Google
-                     setPageScreenState={(_) => props.setPageScreenState(_)}
-                     setMainScreenState={(_) => setMainScreenState(_)}
-                  />,
-                  'Facebook': <Facebook
-                     setPageScreenState={(_) => props.setPageScreenState(_)}
-                     setMainScreenState={(_) => setMainScreenState(_)}
-                  />,
-               }}
-               screen={mainScreenState}
-            />
-         </View>
+  return (
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => {
+        setModalVisible(!modalVisible);
+      }}
+    >
+      <View style={STYLES.page}>
+         
+        <TouchableWithoutFeedback onPress={() => props.setPageScreenState('StartPage')}>
+          <View style={STYLES.modalPadding} />
+        </TouchableWithoutFeedback>
+
+        <View style={STYLES.modalPadding}>
+          <_Header
+            text={terms['0005']}
+            action={() => props.setPageScreenState('StartPage')}
+          />
+          <_Navigator
+            screens={{
+              'EmailPassword': <EmailPassword
+                setPageScreenState={(_) => props.setPageScreenState(_)}
+                setMainScreenState={(_) => setMainScreenState(_)}
+              />,
+              'Google': <Google
+                setPageScreenState={(_) => props.setPageScreenState(_)}
+                setMainScreenState={(_) => setMainScreenState(_)}
+              />,
+              'Facebook': <Facebook
+                setPageScreenState={(_) => props.setPageScreenState(_)}
+                setMainScreenState={(_) => setMainScreenState(_)}
+              />,
+            }}
+            screen={mainScreenState}
+          />
+        </View>
       </View>
+    </Modal>
   );
 };
