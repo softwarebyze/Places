1449689f50a0806ad3c760da013849153c2a5379
kckirch <kckircher@hotmail.com>
2023-07-09 12:32:20 -0600

    changes color to fit naming convention

--- components/Start/elements/_Header.js
+++ components/Start/elements/_Header.js
@@ -1,5 +1,7 @@
 import { View, Pressable, Text } from 'react-native'; // import Text here
 import { Ionicons } from '@expo/vector-icons';
+import COLORS from '../../../settings/Colors';
+
 import STYLES from '../styles/Styles';
 
 export default _Header = props => {
@@ -19,7 +21,7 @@ export default _Header = props => {
         onPress={props.action}
         style={{ padding: 10 }}
       >
-        <Ionicons name="close" size={24} color="black" />
+        <Ionicons name="close" size={24} color= {COLORS.black_070} />
       </Pressable>
     </View>
   );
