2481400b8a8bd89a5e7e204e21e9c665b56eebd1
Simon Cooper <simonac2@illinois.edu>
2023-07-12 15:54:20 -0500

    LocationPage.js is implemented, 'Continue' button navigates to InterestsPage.

--- components/Start/pages/LocationPage.js
+++ components/Start/pages/LocationPage.js
@@ -12,41 +12,58 @@ terms = TERMS["English"];
 export default LocationPage = (props) => {
   return (
     <View>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
-      <Text>Location Page</Text>
 
+<_Button
+          text={terms['0019']}
+          action={() => props.setPageScreenState('LocationPage')}
+          color='primary1_100'
+          borderColor='primary1_100'
+          textColor='white_100'
+          style={STYLES.startButton}
+        />
+       <Text
+          style={STYLES.welcomeText}
+        >
+          {terms['0018']}
+        </Text>
+        <Text
+          style={STYLES.sloganText}
+        >
+          {terms['0020']}
+        </Text>
+        <_Input
+            labelText={'Country'}
+            onFocus={() => setPasswordFocusState(true)}
+            onBlur={() => setPasswordFocusState(false)}
+            borderColor={'primary1_100'}
+            style={STYLES.signUpInput}
+         />
+<_Input
+            labelText={'State / Province'}
+            onFocus={() => setPasswordFocusState(true)}
+            onBlur={() => setPasswordFocusState(false)}
+            borderColor={'primary1_100'}
+            style={STYLES.signUpInput}
+         />
+         <_Input
+            labelText={'City'}
+            onFocus={() => setPasswordFocusState(true)}
+            onBlur={() => setPasswordFocusState(false)}
+            borderColor={'primary1_100'}
+            style={STYLES.signUpInput}
+         />
 
-
+<_Button
+        
+        text={terms['0008']}
+        action={() => props.setPageScreenState('InterestsPage')}
+        color='just_blue'
+        borderColor='primary1_030'
+        textColor='white_100'
+        underline={true}
+        
+        
+     />
 
       
     </View>
