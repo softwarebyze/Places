66cabb7f1b905745f0370b4757690e880a3f71af
Simon Cooper <simonac2@illinois.edu>
2023-07-11 22:06:02 -0500

    added a text import

--- components/Start/pages/LocationPage.js
+++ components/Start/pages/LocationPage.js
@@ -6,7 +6,7 @@ import _Header from "../elements/_Header";
 import _Divider from "../elements/_Divider";
 import STYLES from "../styles/Styles";
 import TERMS from "../../../settings/Terms";
-
+import { Text } from "react-native";
 terms = TERMS["English"];
 
 export default LocationPage = (props) => {
