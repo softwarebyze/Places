6f4cfd73bb9c1cdb66cc4e7c3485de7a60f36b30
softwarebyze <zackebenfeld@gmail.com>
2023-07-16 05:34:07 -0400

    add to home page

--- components/Start/pages/HomePage.js
+++ components/Start/pages/HomePage.js
@@ -1,9 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, Pressable } from "react-native";
+import TERMS from "../../../settings/Terms";
+const terms = TERMS["English"];
+import _Button from "../elements/_Button";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import COLORS from "../../../settings/Colors";
+
 const HomePage = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>HomePage</Text>
-    </View>
+    <SafeAreaView>
+      <Button
+        title={terms["0027"]}
+        onPress={() => navigation.navigate("JoinPlace")}
+        color={COLORS.orange}
+      />
+    </SafeAreaView>
   );
 };
 export default HomePage;
