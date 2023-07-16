0db18b73039e56bf7218de5e401f49b0adf6aacb
softwarebyze <zackebenfeld@gmail.com>
2023-07-16 06:02:27 -0400

    created pages for Category, Maps, Neighbors, & Profile

---
+++ components/Start/pages/NeighborsPage.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const NeighborsPage = () => {
+  return (
+    <View>
+      <Text>NeighborsPage</Text>
+    </View>
+  );
+};
+export default NeighborsPage;
---
+++ components/Start/pages/ProfilePage.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+const ProfilePage = () => {
+  return (
+    <View>
+      <Text>ProfilePage</Text>
+    </View>
+  );
+};
+export default ProfilePage;
