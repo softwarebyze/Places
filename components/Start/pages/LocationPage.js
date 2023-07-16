90efba30ccf9a7d0f1d35b309ab027886ce2dda3
softwarebyze <zackebenfeld@gmail.com>
2023-07-16 04:45:26 -0400

    safe view, remove extra btn

--- components/Start/pages/LocationPage.js
+++ components/Start/pages/LocationPage.js
@@ -8,22 +8,15 @@ import STYLES from "../styles/Styles";
 import TERMS from "../../../settings/Terms";
 import { Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Styles from "../styles/Styles";
 const terms = TERMS["English"];
 
 const LocationPage = () => {
   const navigator = useNavigation();
   return (
-    <View>
-      <_Button
-        text={terms["0019"]}
-        action={() => navigator.navigate("ChooseLocation")}
-        color="primary1_100"
-        borderColor="primary1_100"
-        textColor="white_100"
-        style={STYLES.startButton}
-      />
-      <Text style={STYLES.welcomeText}>{terms["0018"]}</Text>
-      <Text style={STYLES.sloganText}>{terms["0020"]}</Text>
+    <SafeAreaView style={STYLES.page}>
+      <Text style={STYLES.descriptionText}>{terms["0020"]}</Text>
       <_Input
         labelText={"Country"}
         onFocus={() => setPasswordFocusState(true)}
@@ -54,7 +47,7 @@ const LocationPage = () => {
         textColor="white_100"
         underline={true}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 export default LocationPage;
