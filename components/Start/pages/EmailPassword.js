706982c48ac5bd083fde92a063d699df8e85bca8
softwarebyze <zackebenfeld@gmail.com>
2023-07-16 04:33:52 -0400

    remove unused imports

--- components/Start/pages/EmailPassword.js
+++ components/Start/pages/EmailPassword.js
@@ -2,13 +2,10 @@ import { View } from "react-native";
 import { useState } from "react";
 import _Button from "../elements/_Button";
 import _Input from "../elements/_Input";
-import _Header from "../elements/_Header";
-import _Divider from "../elements/_Divider";
 import STYLES from "../styles/Styles";
 import TERMS from "../../../settings/Terms";
 const terms = TERMS["English"];
 import Continues from "./Continues";
-import Details from "./Details";
 
 const validateEmail = (email) => {
   return email.match(
