d524ca420508f612002c410ea3c46730d0c1b84e
softwarebyze <zackebenfeld@gmail.com>
2023-07-19 01:06:07 -0400

    location dropdown

--- components/Start/pages/LocationPage.js
+++ components/Start/pages/LocationPage.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Pressable, TouchableOpacity, View } from "react-native";
 import { useState } from "react";
 import _Button from "../elements/_Button";
 import _Input from "../elements/_Input";
@@ -9,16 +9,67 @@ import TERMS from "../../../settings/Terms";
 import { Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Collapsible from "react-native-collapsible";
+import Colors from "../../../settings/Colors";
 const terms = TERMS["English"];
 
 const LocationPage = () => {
   const navigator = useNavigation();
+  const [isCollapsed, setIsCollapsed] = useState(true);
+  const [location, setLocation] = useState("");
+
+  const toggleDropdown = () => {
+    setIsCollapsed(!isCollapsed);
+  };
   return (
-    <SafeAreaView style={STYLES.page}>
-      <Text style={STYLES.descriptionText}>{terms["0020"]}</Text>
-      <_Input labelText={"Country"} borderColor={"primary1_100"} />
-      <_Input labelText={"State / Province"} borderColor={"primary1_100"} />
-      <_Input labelText={"City"} borderColor={"primary1_100"} />
+    <SafeAreaView style={[STYLES.page, { backgroundColor: Colors.light_grey }]}>
+      <Text style={[STYLES.descriptionText, { marginHorizontal: 30 }]}>
+        {terms["0020"]}
+      </Text>
+      <View style={STYLES.dropdownMargin}>
+        <Pressable onPress={toggleDropdown}>
+          <Text
+            style={[
+              STYLES.dropdownHeader,
+              !isCollapsed ? STYLES.blueBorder : "",
+              location.length > 0 ? STYLES.blackText : "",
+            ]}
+          >
+            {location.length > 0 ? location : "Choose a Location"}
+          </Text>
+        </Pressable>
+        <Collapsible collapsed={isCollapsed}>
+          <View style={[STYLES.blueBorder, STYLES.backgroundWhite]}>
+            <TouchableOpacity
+              style={STYLES.dropdownItem}
+              onPress={() => {
+                setLocation("New York City, USA");
+                toggleDropdown();
+              }}
+            >
+              <Text>New York City, USA</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={STYLES.dropdownItem}
+              onPress={() => {
+                setLocation("Tel Aviv, Israel");
+                toggleDropdown();
+              }}
+            >
+              <Text>Tel Aviv, Israel</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={STYLES.dropdownItem}
+              onPress={() => {
+                setLocation("Herzilya, Israel");
+                toggleDropdown();
+              }}
+            >
+              <Text>Herzilya, Israel</Text>
+            </TouchableOpacity>
+          </View>
+        </Collapsible>
+      </View>
 
       <_Button
         text={terms["0008"]}
--- components/Start/styles/Styles.js
+++ components/Start/styles/Styles.js
@@ -149,4 +149,29 @@ export default new StyleSheet.create({
   whiteText: {
     color: COLORS.white_100,
   },
+  blackText: {
+    color: "black",
+  },
+  dropdownHeader: {
+    width: ELEMENT_WIDTH,
+    padding: 16,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 6,
+    backgroundColor: COLORS.white_100,
+    color: COLORS.dark_grey,
+  },
+  blueBorder: {
+    borderWidth: 1,
+    borderColor: COLORS.primary1_100,
+  },
+  dropdownItem: {
+    width: ELEMENT_WIDTH,
+    padding: 16,
+  },
+  backgroundWhite: {
+    backgroundColor: COLORS.white_100,
+  },
+  dropdownMargin: {
+    marginVertical: 24,
+  },
 });
