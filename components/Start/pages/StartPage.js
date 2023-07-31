17ef5aabf591e395b5e10d5b583b6712a2a107db
Simon Cooper <simonac2@illinois.edu>
2023-07-31 18:04:21 -0500

    created the requested spinner

--- components/Start/pages/StartPage.js
+++ components/Start/pages/StartPage.js
@@ -62,13 +62,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    textAlign: "center",
-    paddingTop: 30,
-    backgroundColor: "#ecf0f1",
-    padding: 8,
   },
-  spinnerTextStyle: {
-    color: "#FFF",
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
   },
 });
 
