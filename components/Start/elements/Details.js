b078f798e7e948b48c2c67feb03cf958a9f206da
softwarebyze <zackebenfeld@gmail.com>
2023-08-01 00:11:05 -0400

    fixed view with keyboard issue (android)

--- components/Start/elements/Details.js
+++ components/Start/elements/Details.js
@@ -9,7 +9,7 @@ import TERMS from "../../../settings/Terms";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PhoneInput from "react-native-phone-input";
-import { Platform, Text, View } from "react-native";
+import { KeyboardAvoidingView, Platform, Text, View } from "react-native";
 import { useRef, useState } from "react";
 import Colors from "../../../settings/Colors";
 import DateTimePicker from "@react-native-community/datetimepicker";
@@ -44,85 +44,91 @@ const Details = () => {
   const navigator = useNavigation();
   return (
     <SafeAreaView style={[Styles.suliContinues, Styles.page]}>
-      <_Input
-        labelText={"First Name"}
-        borderColor={"primary1_100"}
-        style={Styles.signUpInput}
-        onChangeText={setFirstName}
-      />
-      <_Input
-        labelText={"Last Name"}
-        borderColor={"primary1_100"}
-        style={Styles.signUpInput}
-        onChangeText={setLastName}
-      />
-      <View style={Styles.signUpInput}>
-        <Text style={[Styles.d1Box, Styles.inputLabel]}>{"Phone Number"}</Text>
-        <View
-          style={[
-            Styles.d2Box,
-            {
-              borderRadius: 10,
-              padding: 10,
-              fontSize: 17,
-              borderColor: Colors.primary1_100,
-              justifyContent: "center",
-            },
-          ]}
-        >
-          <PhoneInput
-            initialCountry="us"
-            ref={phoneRef}
-            onChangePhoneNumber={setPhoneNumber}
-          />
-        </View>
-      </View>
-      <_Dropdown
-        label="Select an option"
-        options={genders}
-        labelText="Gender"
-        onSelect={setGender}
-      />
-      <View style={Styles.signUpInput}>
-        <Text style={[Styles.d1Box, Styles.inputLabel]}>{"Date of Birth"}</Text>
-        <View
-          style={[
-            Styles.d2Box,
-            {
-              flexDirection: "row",
-              borderRadius: 10,
-              fontSize: 17,
-              borderColor: Colors.primary1_100,
-              justifyContent: "flex-start",
-              alignItems: "center",
-              padding: 12,
-              height: 70,
-            },
-          ]}
-        >
-          {/* Added platform code because there's a bug in Android 
-          where the date picker won't hide */}
-          {Platform.OS === "ios" ? (
-            <DateTimePicker value={dateOfBirth} onChange={onChangeDate} />
-          ) : showDatePicker ? (
-            <DateTimePicker value={dateOfBirth} onChange={onChangeDate} />
-          ) : (
-            <Button
-              title={dateOfBirth.toLocaleDateString()}
-              onPress={() => setShowDatePicker(true)}
+      <KeyboardAvoidingView>
+        <_Input
+          labelText={"First Name"}
+          borderColor={"primary1_100"}
+          style={Styles.signUpInput}
+          onChangeText={setFirstName}
+        />
+        <_Input
+          labelText={"Last Name"}
+          borderColor={"primary1_100"}
+          style={Styles.signUpInput}
+          onChangeText={setLastName}
+        />
+        <View style={Styles.signUpInput}>
+          <Text style={[Styles.d1Box, Styles.inputLabel]}>
+            {"Phone Number"}
+          </Text>
+          <View
+            style={[
+              Styles.d2Box,
+              {
+                borderRadius: 10,
+                padding: 10,
+                fontSize: 17,
+                borderColor: Colors.primary1_100,
+                justifyContent: "center",
+              },
+            ]}
+          >
+            <PhoneInput
+              initialCountry="us"
+              ref={phoneRef}
+              onChangePhoneNumber={setPhoneNumber}
             />
-          )}
+          </View>
         </View>
-      </View>
-      <_Button
-        text={terms["0017"]}
-        action={() => navigator.navigate("ChooseLocation")}
-        color={disabled ? "primary1_030" : "primary1_100"}
-        borderColor={disabled ? "light_grey" : "primary1_100"}
-        textColor="white_100"
-        underline={false}
-        disabled={disabled}
-      />
+        <_Dropdown
+          label="Select an option"
+          options={genders}
+          labelText="Gender"
+          onSelect={setGender}
+        />
+        <View style={Styles.signUpInput}>
+          <Text style={[Styles.d1Box, Styles.inputLabel]}>
+            {"Date of Birth"}
+          </Text>
+          <View
+            style={[
+              Styles.d2Box,
+              {
+                flexDirection: "row",
+                borderRadius: 10,
+                fontSize: 17,
+                borderColor: Colors.primary1_100,
+                justifyContent: "flex-start",
+                alignItems: "center",
+                padding: 12,
+                height: 70,
+              },
+            ]}
+          >
+            {/* Added platform code because there's a bug in Android 
+          where the date picker won't hide */}
+            {Platform.OS === "ios" ? (
+              <DateTimePicker value={dateOfBirth} onChange={onChangeDate} />
+            ) : showDatePicker ? (
+              <DateTimePicker value={dateOfBirth} onChange={onChangeDate} />
+            ) : (
+              <Button
+                title={dateOfBirth.toLocaleDateString()}
+                onPress={() => setShowDatePicker(true)}
+              />
+            )}
+          </View>
+        </View>
+        <_Button
+          text={terms["0017"]}
+          action={() => navigator.navigate("ChooseLocation")}
+          color={disabled ? "primary1_030" : "primary1_100"}
+          borderColor={disabled ? "light_grey" : "primary1_100"}
+          textColor="white_100"
+          underline={false}
+          disabled={disabled}
+        />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
