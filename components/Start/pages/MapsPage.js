325599fd6c5a4e4542aace1bd5c78e0003dfdadc
softwarebyze <zackebenfeld@gmail.com>
2023-08-01 01:04:26 -0400

    working map view

--- components/Start/pages/MapsPage.js
+++ components/Start/pages/MapsPage.js
@@ -1,9 +1,21 @@
-import { View, Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import MapView from "react-native-maps";
+import { View } from "react-native";
+// To deploy, follow https://docs.expo.dev/versions/latest/sdk/map-view/#deploy-app-with-google-maps
+
 const MapsPage = () => {
   return (
     <View>
-      <Text>MapsPage</Text>
+      <MapView style={styles.map} />
     </View>
   );
 };
 export default MapsPage;
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
