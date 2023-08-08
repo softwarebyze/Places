47ee1b5ae3b98814a3ff29f243419730be27fbf7
brett1185 <brett_currier@hotmail.com>
2023-08-08 09:16:48 -0400

    added images to all interests, changed SafeAreaView to just View on applicaple pages
    Co-authored-by: Zachary Ebenfeld <softwarebyze@users.noreply.github.com>

--- data.js
+++ data.js
@@ -12,42 +12,48 @@ export const interests = [
     category: "buy/sell",
     location: "New York City",
     members: 150,
-    image: "/assets/interest_images/baseball.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fbaseball.png?alt=media&token=910ae20f-a427-401d-8742-39cdaf177c3e",
   },
   {
     name: "basketball",
     category: "donations",
     location: "New York City",
     members: 115,
-    image: "/assets/interest_images/basketball.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fbasketball.png?alt=media&token=7fd3ba7e-63f9-4b25-9356-659545453abe",
   },
   {
     name: "boxing",
     category: "hobbies",
     location: "New York City",
     members: 3,
-    image: "/assets/interest_images/boxing.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fboxing.png?alt=media&token=af590d26-f137-461d-be7d-dd7a5da5288f",
   },
   {
     name: "climbing",
     category: "sports",
     location: "New York City",
     members: 7,
-    image: "/assets/interest_images/climbing.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fclimbing.png?alt=media&token=6a7565b3-ffd6-4ed0-86b1-09b445dbaf72",
   },
   {
     name: "cycling",
     category: "buy/sell",
     location: "New York City",
     members: 1500,
-    image: "/assets/interest_images/cycling.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fcycling.png?alt=media&token=96ed8153-e902-4760-9dcb-c7cfdb9398b0",
   },
   {
     name: "golf",
     category: "donations",
     location: "New York City",
     members: 68,
-    image: "/assets/interest_images/golf.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fgolf.png?alt=media&token=8481d03d-68fc-42e9-9cef-2754b87810f1",
   },
   {
     name: "gymnastics",
@@ -62,55 +68,63 @@ export const interests = [
     category: "sports",
     location: "New York City",
     members: 100,
-    image: "/assets/interest_images/hockey.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fhockey.png?alt=media&token=287edf83-1041-40ac-810c-659d0a705114",
   },
   {
     name: "skateboarding",
     category: "buy/sell",
     location: "New York City",
     members: 151,
-    image: "/assets/interest_images/skateboarding.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fskateboarding.png?alt=media&token=dd3d25c6-40fd-4b9e-851e-3492c63c303f",
   },
   {
     name: "skiing",
     category: "donations",
     location: "New York City",
     members: 15,
-    image: "/assets/interest_images/skiing.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fskiing.png?alt=media&token=21159781-9680-4fa6-a6d9-0a633888185c",
   },
   {
     name: "rugby",
     category: "hobbies",
     location: "New York City",
     members: 35,
-    image: "/assets/interest_images/rugby.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Frugby.png?alt=media&token=c7f3eba6-bb3d-4101-a411-5fb26097c2c9",
   },
   {
     name: "soccer",
     category: "sports",
     location: "New York City",
     members: 68,
-    image: "/assets/interest_images/soccer.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fsoccer.png?alt=media&token=1fce68af-c191-4423-9167-4f0fb02d568a",
   },
   {
     name: "surfing",
     category: "buy/sell",
     location: "New York City",
     members: 82,
-    image: "/assets/interest_images/surfing.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fsurfing.png?alt=media&token=1fe7b0b9-2777-4a03-a2d1-97d25649feb3",
   },
   {
     name: "swimming",
     category: "donations",
     location: "New York City",
     members: 99,
-    image: "/assets/interest_images/swimming.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fswimming.png?alt=media&token=1d6ce8b4-f807-4359-b194-7bd67b1f4fe2",
   },
   {
     name: "volleyball",
     category: "hobbies",
     location: "New York City",
     members: 1000,
-    image: "/assets/interest_images/volleyball.png",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/places-e6047.appspot.com/o/interest_images%2Fvolleyball.png?alt=media&token=3b9e1cdf-de4d-4099-8ffc-910cff98115f",
   },
 ];
