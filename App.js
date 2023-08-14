9f69e6afb56cc13beab97a53e7c027c6668b0774
softwarebyze <zackebenfeld@gmail.com>
2023-08-14 16:05:12 -0400

    changed navigation folder to lowercase to match other folder names

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ import { Foundation } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import HomeTabs from "./components/Start/Navigation/HomeTabs";
+import HomeTabs from "./components/Start/navigation/HomeTabs";
 import { OverlayProvider } from "stream-chat-expo";
 
 const Stack = createNativeStackNavigator();
