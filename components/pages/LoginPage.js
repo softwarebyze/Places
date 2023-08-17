cdf6ec076a2478595924c0a54f4ca20c1547bd61
simonac2-gif <80356023+simonac2-gif@users.noreply.github.com>
2023-08-16 20:03:14 -0500

    Update LoginPage.js
    main should now recognize unwanted passwords

--- components/pages/LoginPage.js
+++ components/pages/LoginPage.js
@@ -1,18 +1,18 @@
 import { ActivityIndicator } from "react-native";
 import _Header from "../elements/_Header";
 import STYLES from "../styles/Styles";
-import TERMS from "../../settings/Terms";
+import TERMS from "../../../settings/Terms";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 import _Button from "../elements/_Button";
 import _Input from "../elements/_Input";
 import _Divider from "../elements/_Divider";
-
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { StreamChat } from "stream-chat";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "../../firebaseConfig";
+import { db } from "../../../firebaseConfig";
+import Colors from "../../../settings/Colors";
 
 const terms = TERMS["English"];
 
@@ -21,23 +21,11 @@ const validateEmail = (email) => {
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   );
 };
-
 const validatePassword = (password) => {
   return password.length >= 6;
 };
-
 const { EXPO_PUBLIC_STREAM_API_KEY } = process.env;
 const client = StreamChat.getInstance(EXPO_PUBLIC_STREAM_API_KEY);
-
-const signIn = async (email, password) => {
-  const auth = getAuth();
-  await signInWithEmailAndPassword(auth, email, password);
-  const userId = auth.currentUser.uid;
-  const res = await fetch(`https://auth-token.onrender.com/${userId}`);
-  const { token } = await res.json();
-  await client.connectUser({ id: userId }, token);
-};
-
 const LoginPage = () => {
   const navigator = useNavigation();
   const [emailFocusState, setEmailFocusState] = useState(false);
@@ -45,18 +33,41 @@ const LoginPage = () => {
   const [passwordFocusState, setPasswordFocusState] = useState(false);
   const [passwordTextState, setPasswordTextState] = useState("");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const emailIsValid = validateEmail(emailTextState);
   const passwordIsValid = validatePassword(passwordTextState);
   const canContinue = emailIsValid && passwordIsValid;
 
+  const signIn = async () => {
+    const auth = getAuth();
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        emailTextState,
+        passwordTextState,
+      );
+      const userId = auth.currentUser.uid;
+      const res = await fetch(`https://auth-token.onrender.com/${userId}`);
+      const { token } = await res.json();
+      await client.connectUser({ id: userId }, token);
+      return user;
+    } catch (error) {
+      if (error.code === "auth/wrong-password") {
+        setError(terms["incorrect_password"]);
+      } else if (error.code === "auth/too-many-requests") {
+        setError(terms["too_many_attempts_try_again_later"]);
+      } else {
+        throw new Error(error);
+      }
+    }
+  };
+
   const getUserData = async () => {
     const auth = getAuth();
     const userId = auth.currentUser.uid;
-
     const userRef = doc(db, "users", userId);
     const userSnap = await getDoc(userRef);
-
     if (userSnap.exists()) {
       return userSnap.data();
     } else {
@@ -67,7 +78,9 @@ const LoginPage = () => {
 
   const handleSignInFlow = async () => {
     setLoading(true);
-    await signIn(emailTextState, passwordTextState);
+    const user = await signIn();
+    if (!user) return setLoading(false);
+
     const userData = await getUserData();
     setLoading(false);
     if (!userData?.details_completed) return navigator.replace("Details");
@@ -78,7 +91,6 @@ const LoginPage = () => {
       });
     return navigator.replace("HomeTabs");
   };
-
   return (
     <SafeAreaView style={STYLES.page}>
       <_Header
@@ -109,7 +121,14 @@ const LoginPage = () => {
       <_Input
         secureTextEntry={true}
         labelText={terms["0007"]}
-        subtextText={terms["0015"]}
+
+        subtextText={
+          passwordTextState.length < 6
+            ? terms["your_password_must_be_at_least_6_characters"]
+            : error.length
+            ? error
+            : ""
+        }
         onFocus={() => setPasswordFocusState(true)}
         onBlur={() => setPasswordFocusState(false)}
         onChangeText={(input) => setPasswordTextState(input)}
@@ -121,55 +140,56 @@ const LoginPage = () => {
             : "primary1_030"
         }
         subtextColor={
-          emailTextState && !passwordIsValid
-            ? "primary1_030"
-            : passwordFocusState
-            ? "primary1_030"
-            : "clear_000"
-        }
-      />
-      {loading ? (
-        <ActivityIndicator />
-      ) : (
-        <>
-          <_Button
-            text={terms["0008"]}
-            action={handleSignInFlow}
-            color={canContinue ? "primary1_100" : "primary1_030"}
-            borderColor={canContinue ? "primary1_100" : "primary1_030"}
-            textColor="white_100"
-            disabled={!canContinue}
-          />
-          <_Divider text="or" color="gray1_100" />
-          <_Button
-            text={terms["0011"]}
-            action={async () => {
-              navigator.replace("HomeTabs");
-            }}
-            color="primary1_100"
-            borderColor="primary1_100"
-            textColor="white_100"
-            style={{ marginBottom: 20 }}
-          />
-          <_Button
-            text={terms["0012"]}
-            action={() => navigator.replace("Details")}
-            color="white_100"
-            borderColor="primary1_100"
-            textColor="primary1_100"
-          />
-          <_Button
-            text={terms["0013"]}
-            action={() => navigator.replace("Signup")}
-            color="white_100"
-            borderColor="white_100"
-            textColor="primary1_100"
-            underline={true}
-          />
-        </>
-      )}
-    </SafeAreaView>
-  );
+          error.length
+          ? "error_100"
+          : emailTextState && !passwordIsValid
+          ? "primary1_030"
+          : passwordFocusState
+          ? "primary1_030"
+          : "clear_000"
+      }
+    />
+    {loading ? (
+      <ActivityIndicator />
+    ) : (
+      <>
+        <_Button
+          text={terms["0008"]}
+          action={handleSignInFlow}
+          color={canContinue ? "primary1_100" : "primary1_030"}
+          borderColor={canContinue ? "primary1_100" : "primary1_030"}
+          textColor="white_100"
+          disabled={!canContinue}
+        />
+        <_Divider text="or" color="gray1_100" />
+        <_Button
+          text={terms["0011"]}
+          action={async () => {
+            navigator.replace("HomeTabs");
+          }}
+          color="primary1_100"
+          borderColor="primary1_100"
+          textColor="white_100"
+          style={{ marginBottom: 20 }}
+        />
+        <_Button
+          text={terms["0012"]}
+          action={() => navigator.replace("Details")}
+          color="white_100"
+          borderColor="primary1_100"
+          textColor="primary1_100"
+        />
+        <_Button
+          text={terms["0013"]}
+          action={() => navigator.replace("Signup")}
+          color="white_100"
+          borderColor="white_100"
+          textColor="primary1_100"
+          underline={true}
+        />
+      </>
+    )}
+  </SafeAreaView>
+);
 };
-
 export default LoginPage;
