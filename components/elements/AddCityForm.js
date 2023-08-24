4cbb48be772d060d728d3fd70df3fe6d325cfba6
Avraham <ayschochet@gmail.com>
2023-08-24 16:58:44 +0300

    add city button is functional

--- components/elements/AddCityForm.js
+++ components/elements/AddCityForm.js
@@ -1,46 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Styles from "../styles/Styles";
+import Colors from "../../settings/Colors";
 import { getAuth } from "firebase/auth";
 import { db } from "../../firebaseConfig";
-import { doc, getDoc } from "firebase/firestore";
+import { arrayUnion, doc, getDoc, updateDoc } from "firebase/firestore";
 import CitiesDropdown from "./CitiesDropdown";
+import _Button from "./_Button";
 
 const AddCityForm = () => {
   const [city, setCity] = useState(null);
   const [usersCities, setUsersCities] = useState([]);
+  const fetchUsersCities = async () => {
+    try {
+      const auth = getAuth();
+      const userId = auth.currentUser?.uid;
+      const userRef = doc(db, "users", userId);
+      const userSnap = await getDoc(userRef);
+      console.log("cities:", userSnap.data().cities);
+      if (userSnap.exists()) {
+        setUsersCities(() => {
+          const cities = userSnap.data().cities || [userSnap.data().location];
+          return cities.map((city, i) => ({
+            city: city,
+            id: Date.now() + i + Math.random(),
+          }));
+        });
+      } else {
+        throw new Error("No user found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleAddCity = async () => {
+    try {
+      const auth = getAuth();
+      const userId = auth.currentUser?.uid;
+      const userRef = doc(db, "users", userId);
+      if (!city) throw new Error("No city selected");
+      if (usersCities.find((userCity) => userCity.city === city))
+        throw new Error("City already added");
+      await updateDoc(userRef, {
+        cities: arrayUnion(city),
+      });
+      await fetchUsersCities();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
-    const fetchUsersCities = async () => {
-      try {
-        const auth = getAuth();
-        const userId = auth.currentUser?.uid;
-        const userRef = doc(db, "users", userId);
-        const userSnap = await getDoc(userRef);
-        console.log("cities:", userSnap.data().cities);
-        if (userSnap.exists()) {
-          setUsersCities(() => {
-            const cities = userSnap.data().cities || [userSnap.data().location];
-            return cities.map((city, i) => ({
-              city: city,
-              id: Date.now() + i + Math.random(),
-            }));
-          });
-        } else {
-          throw new Error("No user found");
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    };
     fetchUsersCities();
   }, []);
+
   return (
     <View>
       <Text style={Styles.groupLabelText}>Add a City</Text>
       <CitiesDropdown onSelect={setCity} />
+      <_Button action={handleAddCity} text="Add City" disabled={!city} />
       <Text style={Styles.groupLabelText}>Your Cities</Text>
-      <View style={{ display: "flex", alignItems: "flex-start", padding: 5 }}>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "flex-start",
+          padding: 5,
+          flexWrap: "wrap",
+          flexDirection: "row",
+          gap: 10,
+        }}
+      >
         {usersCities.map(({ city, id }) => (
           <View style={styles.usersCityButton} key={id}>
             <Text style={styles.usersCityButtonText}>{city}</Text>
@@ -50,19 +79,9 @@ const AddCityForm = () => {
     </View>
   );
 };
+export default AddCityForm;
 
 const styles = StyleSheet.create({
-  addACity: {
-    borderWidth: 2,
-    padding: 8,
-    borderRadius: 6,
-    borderColor: Colors.dark_grey,
-  },
-  addACityText: {
-    color: Colors.dark_grey,
-    fontSize: 14,
-    fontWeight: "600",
-  },
   usersCityButton: {
     borderWidth: 1.5,
     padding: 12,
@@ -81,4 +100,3 @@ const styles = StyleSheet.create({
     fontSize: 25,
   },
 });
-export default AddCityForm;
