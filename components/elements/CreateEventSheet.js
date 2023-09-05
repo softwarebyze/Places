4d5fdc7cd956f74bf522a0b56e9b71b0187c1d2c
brett1185 <brett_currier@hotmail.com>
2023-09-05 16:13:41 -0400

    added some styling, began creating add tag function

--- components/elements/CreateEventSheet.js
+++ components/elements/CreateEventSheet.js
@@ -1,5 +1,5 @@
 import BottomSheet from "@gorhom/bottom-sheet";
-import React, { useMemo, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Button, View, Text } from "react-native";
 import SheetHeader from "./SheetHeader";
 import _Input from "./_Input";
@@ -8,6 +8,10 @@ import _Button from "./_Button";
 import Styles from "../styles/Styles";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import Colors from "../../settings/Colors";
+import { AntDesign } from "@expo/vector-icons";
+import { StreamChat } from "stream-chat";
+
+const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
 const CreateEventSheet = () => {
   const createEventBottomSheetRef = useRef(null);
@@ -17,6 +21,7 @@ const CreateEventSheet = () => {
   const [timeOfEvent, setTimeOfEvent] = useState(new Date());
   const [showDate, setShowDate] = useState(false);
   const [showTime, setShowTime] = useState(false);
+  const tagList = [];
 
   const GooglePlacesInput = () => {
     return (
@@ -34,6 +39,24 @@ const CreateEventSheet = () => {
     );
   };
 
+  useEffect(() => {
+    const fetchTags = async () => {
+      for (let i = 0; i < 3; i++) {
+        const tags = await client.queryChannels(
+          {
+            type: "team",
+          },
+          {},
+          { limit: 30, offset: 30 * i },
+        );
+        tagList.push(...tags);
+      }
+      const allTags = tagList.map((c) => c.data.interest);
+      console.log(allTags);
+    };
+    fetchTags();
+  }, []);
+
   const onChangeDateOfEvent = (eventDate, selectedDate) => {
     const currentDate = selectedDate || dateOfEvent;
     setShowDate(false);
@@ -50,7 +73,7 @@ const CreateEventSheet = () => {
       snapPoints={snapPoints}
       enablePanDownToClose
     >
-      <View style={[Styles.page, { gap: 10 }]}>
+      <View style={Styles.page}>
         <SheetHeader sheetHeaderText={headerText} />
 
         <_Input
@@ -65,48 +88,62 @@ const CreateEventSheet = () => {
           placeholder="Location"
           placeholderTextColor="primary1_030"
         />
-
-        <View>
-          <View style={{ flexDirection: "row" }}>
-            <View style={Styles.signUpInput}>
-              <Text style={[Styles.inputLabel]}>{"Date"}</Text>
-              <View
-                style={[
-                  Styles.d2Box,
-                  {
-                    flexDirection: "row",
-                    borderRadius: 10,
-                    fontSize: 17,
-                    borderColor: Colors.primary1_100,
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    padding: 12,
-                    height: 35,
-                    width: 160,
-                  },
-                ]}
-              >
-                {/* Added platform code because there's a bug in Android 
+        <View
+          style={{
+            gap: 15,
+            flexDirection: "row",
+          }}
+        >
+          <View>
+            <View>
+              <View style={Styles.signUpInput}>
+                <Text style={[Styles.inputLabel]}>{"Date"}</Text>
+                <View
+                  style={[
+                    Styles.d2Box,
+                    {
+                      flexDirection: "row",
+                      borderRadius: 10,
+                      fontSize: 17,
+                      borderColor: Colors.primary1_100,
+                      justifyContent: "flex-start",
+                      alignItems: "center",
+                      padding: 12,
+                      height: 40,
+                      width: 160,
+                    },
+                  ]}
+                >
+                  <AntDesign
+                    name="calendar"
+                    size={18}
+                    style={{
+                      color: Colors.primary1_100,
+                    }}
+                  />
+                  {/* Added platform code because there's a bug in Android 
           where the date picker won't hide */}
-                {Platform.OS === "ios" ? (
-                  <DateTimePicker
-                    value={dateOfEvent}
-                    onChange={onChangeDateOfEvent}
-                  />
-                ) : showDatePicker ? (
-                  <DateTimePicker
-                    value={dateOfEvent}
-                    onChange={onChangeDateOfEvent}
-                  />
-                ) : (
-                  <Button
-                    title={dateOfEvent.toLocaleDateString()}
-                    onPress={() => setShowDate(true)}
-                  />
-                )}
+                  {Platform.OS === "ios" ? (
+                    <DateTimePicker
+                      value={dateOfEvent}
+                      onChange={onChangeDateOfEvent}
+                    />
+                  ) : showDatePicker ? (
+                    <DateTimePicker
+                      value={dateOfEvent}
+                      onChange={onChangeDateOfEvent}
+                    />
+                  ) : (
+                    <Button
+                      title={dateOfEvent.toLocaleDateString()}
+                      onPress={() => setShowDate(true)}
+                    />
+                  )}
+                </View>
               </View>
             </View>
-
+          </View>
+          <View>
             <View style={Styles.signUpInput}>
               <Text style={[Styles.inputLabel]}>{"Time"}</Text>
               <View
@@ -120,11 +157,18 @@ const CreateEventSheet = () => {
                     justifyContent: "flex-start",
                     alignItems: "center",
                     padding: 12,
-                    height: 35,
+                    height: 40,
                     width: 160,
                   },
                 ]}
               >
+                <AntDesign
+                  name="clockcircleo"
+                  size={18}
+                  style={{
+                    color: Colors.primary1_100,
+                  }}
+                />
                 {/* Added platform code because there's a bug in Android 
           where the date picker won't hide */}
                 {Platform.OS === "ios" ? (
@@ -147,12 +191,23 @@ const CreateEventSheet = () => {
             </View>
           </View>
         </View>
-        <_Input
-          labelText="Tags"
-          borderColor="primary1_100"
-          placeholder="Tags"
-          placeholderTextColor="primary1_030"
-        />
+        <View style={{ flexDirection: "row" }}>
+          <_Input
+            labelText="Tags"
+            borderColor="primary1_100"
+            placeholder="Tags"
+            placeholderTextColor="primary1_030"
+          />
+          <Button
+            title="Add"
+            style={{
+              borderWidth: 1,
+              borderColor: Colors.secondary_100,
+              color: Colors.secondary_100,
+              justifyContent: "center",
+            }}
+          />
+        </View>
         <_Input
           labelText="Description"
           borderColor="primary1_100"
@@ -163,7 +218,7 @@ const CreateEventSheet = () => {
           subtextAlignSelf="flex-end"
         />
         <_Button text="Create Event" />
-        <_Button text="Cancel" type="secondary" />
+        <_Button text="Cancel" type="secondary" style={{ marginTop: 15 }} />
       </View>
     </BottomSheet>
   );
