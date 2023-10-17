7c4094ff20945a8944dfb182b3c4227560046458
softwarebyze <zackebenfeld@gmail.com>
2023-10-17 13:11:26 -0400

    fixed imports

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { getAuth, initializeAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, initializeAuth } from "firebase/auth";
 import { getReactNativePersistence } from "firebase/auth/react-native";
-import { getFirestore, initializeFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 import { getFunctions, httpsCallable } from "firebase/functions";
 
 // Initialize Firebase
