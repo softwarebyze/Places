68ccc6dce5a1d056adf42f9e5e92f0f0c0835637
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-03-02 16:19:06 -0500

    u can change the display name but it doesnt update the ui right away

--- components/pages/ProfilePage.tsx
+++ components/pages/ProfilePage.tsx
@@ -1,10 +1,13 @@
 import auth from "@react-native-firebase/auth";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import { View, Text } from "react-native";
 import { StreamChat } from "stream-chat";
 
+import { getStreamUserToken } from "../../firebaseConfig";
 import TERMS from "../../settings/Terms";
 import _Button from "../elements/_Button";
+import _Input from "../elements/_Input";
 import STYLES from "../styles/Styles";
 
 const terms = TERMS["English"];
@@ -12,6 +15,7 @@ const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
 
 const ProfilePage = () => {
   const navigator = useNavigation();
+  const [newDisplayName, setNewDisplayName] = useState("");
 
   const logout = () => {
     auth().signOut();
@@ -19,11 +23,34 @@ const ProfilePage = () => {
     navigator.navigate("Start");
   };
 
+  const changeUsername = async () => {
+    await client.disconnectUser();
+    const tokenResponse = await getStreamUserToken();
+    const token = tokenResponse.data.toString();
+    await client.connectUser(
+      {
+        id: auth().currentUser.uid,
+        name: `${newDisplayName}`,
+      },
+      token,
+    );
+  };
+
   return (
     <View style={STYLES.page}>
-      <Text>Display Name: {auth().currentUser.displayName}</Text>
       <Text>Email: {auth().currentUser.email}</Text>
       <Text>Client name: {client?.user.name}</Text>
+      <_Input
+        labelText="Display Name"
+        placeholder={client?.user.name}
+        value={newDisplayName}
+        onChangeText={setNewDisplayName}
+      />
+      <_Button
+        style={{ marginVertical: 8 }}
+        text="Save Display Name"
+        action={changeUsername}
+      />
       <_Button text={terms["0034"]} action={logout} />
     </View>
   );
