157f511be2f6341a505188b295fc2c939b38bb68
Zachary Ebenfeld <zackebenfeld@gmail.com>
2024-03-18 20:15:27 -0400

    wip reply privately

--- components/pages/PlacesChatPage.tsx
+++ components/pages/PlacesChatPage.tsx
@@ -5,6 +5,9 @@ import {
   MessageList,
   MessageInput,
   OverlayProvider,
+  useChatContext,
+  messageActions as defaultMessageActions,
+  ShareRightArrow as ShareRightArrowIcon,
 } from "stream-chat-expo";
 
 import { PlacesChatPageProps } from "../navigation/types";
@@ -13,9 +16,45 @@ const PlacesChatPage = () => {
   const route = useRoute<PlacesChatPageProps["route"]>();
   const navigator = useNavigation<PlacesChatPageProps["navigation"]>();
   const { channel } = route.params;
+  const { client } = useChatContext();
   return (
     <OverlayProvider>
-      <Channel channel={channel}>
+      <Channel
+        forceAlignMessages="left"
+        channel={channel}
+        // custom message action docs:
+        // https://getstream.io/chat/docs/sdk/reactnative/guides/customize-message-actions/#how-to-add-a-custom-message-action
+
+        messageActions={(param) => {
+          const { isMyMessage, ownCapabilities, dismissOverlay, message } =
+            param;
+          const actions = defaultMessageActions({ ...param });
+
+          if (!isMyMessage) {
+            actions.push({
+              action: async () => {
+                dismissOverlay();
+                // ADD REPLY PRIVATELY HERE
+
+                // if (message.user?.id) {
+                //   if (isMuted) {
+                //     console.log("unmuting user");
+
+                //   } else {
+                //     console.log("unmuting user");
+                //     await client.muteUser(message.user.id);
+                //   }
+                // }
+              },
+              actionType: "replyPrivately",
+              icon: <ShareRightArrowIcon />,
+              title: "Reply Privately",
+            });
+          }
+
+          return actions;
+        }}
+      >
         <MessageList
           onThreadSelect={(thread) => {
             navigator.navigate("Thread", { channel, thread });
